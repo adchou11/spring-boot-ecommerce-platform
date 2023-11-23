9e35004c30b981136e691b7e718ff224080a8874
adchou11 <adchou11@gmail.com>
2023-11-23 10:36:29 -0600

    eureka build set up

---
+++ microservices-new-parent-dir/microservices-new/inventory-service/src/main/java/com/adchou/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,24 @@
+package com.adchou.inventoryservice.controller;
+
+import com.adchou.inventoryservice.dto.InventoryResponse;
+import com.adchou.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    // We make this return a list rather than a boolean because we'd rather make one HTTP get request for a set of items rather than N requests
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
